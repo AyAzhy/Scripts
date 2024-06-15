47dcdd64eea83f97a8537c918a4f007060336cef
AyAz <111522724+AyAzhy@users.noreply.github.com>
2024-06-15 14:22:47 +0300

    Update partspammer.lua

--- partspammer.lua
+++ partspammer.lua
@@ -25,8 +25,8 @@ Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.new(0.133333, 0.133333, 0.133333)
 Frame.BorderColor3 = Color3.new(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.333083272, 0, 0.232009932, 0)
-Frame.Size = UDim2.new(0, 340, 0, 301)
+Frame.Position = UDim2.new(0.333083302, 0, 0.232009917, 0)
+Frame.Size = UDim2.new(0, 251, 0, 262)
 
 UICorner.Parent = Frame
 
@@ -35,30 +35,30 @@ TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
 TextLabel.BackgroundTransparency = 1
 TextLabel.BorderColor3 = Color3.new(0, 0, 0)
 TextLabel.BorderSizePixel = 0
-TextLabel.Size = UDim2.new(0, 340, 0, 43)
+TextLabel.Size = UDim2.new(0, 251, 0, 37)
 TextLabel.Font = Enum.Font.PermanentMarker
 TextLabel.Text = "Part Spammer"
 TextLabel.TextColor3 = Color3.new(1, 1, 1)
-TextLabel.TextSize = 55
+TextLabel.TextSize = 48
 
 TextButton.Parent = Frame
 TextButton.BackgroundColor3 = Color3.new(0.133333, 0.133333, 0.133333)
 TextButton.BorderColor3 = Color3.new(0, 0, 0)
 TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.197058827, 0, 0.21262458, 0)
-TextButton.Size = UDim2.new(0, 209, 0, 163)
+TextButton.Position = UDim2.new(0.197665513, 0, 0.211562932, 0)
+TextButton.Size = UDim2.new(0, 154, 0, 142)
 TextButton.Font = Enum.Font.FredokaOne
 TextButton.Text = "SPAM!"
 TextButton.TextColor3 = Color3.new(1, 1, 1)
-TextButton.TextSize = 80
+TextButton.TextSize = 60
 
 TextLabel_2.Parent = Frame
 TextLabel_2.BackgroundColor3 = Color3.new(1, 1, 1)
 TextLabel_2.BackgroundTransparency = 1
 TextLabel_2.BorderColor3 = Color3.new(0, 0, 0)
 TextLabel_2.BorderSizePixel = 0
-TextLabel_2.Position = UDim2.new(0, 0, 0.833887041, 0)
-TextLabel_2.Size = UDim2.new(0, 340, 0, 50)
+TextLabel_2.Position = UDim2.new(-0.0119521916, 0, 0.833540261, 0)
+TextLabel_2.Size = UDim2.new(0, 251, 0, 43)
 TextLabel_2.Font = Enum.Font.LuckiestGuy
 TextLabel_2.Text = "Made By AyAzhy"
 TextLabel_2.TextColor3 = Color3.new(1, 1, 1)
@@ -66,7 +66,7 @@ TextLabel_2.TextSize = 30
 
 -- Scripts
 
-local function BKEH_fake_script() -- TextButton.LocalScript 
+local function KJOZCIH_fake_script() -- TextButton.LocalScript 
 	local script = Instance.new('LocalScript', TextButton)
 
 	local textButton = script.Parent 
@@ -93,11 +93,11 @@ local function BKEH_fake_script() -- TextButton.LocalScript
 	
 	
 end
-coroutine.wrap(BKEH_fake_script)()
-local function VZEGHP_fake_script() -- Frame.LocalScript 
+coroutine.wrap(KJOZCIH_fake_script)()
+local function ELFC_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	script.Parent.Draggable = true
 	script.Parent.Active = true
 end
-coroutine.wrap(VZEGHP_fake_script)()
+coroutine.wrap(ELFC_fake_script)()
